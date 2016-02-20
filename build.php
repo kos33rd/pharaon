96d852980e283962e48b48d10c396131254fc03f
Лобов Константин <lobovke@modeltech.ru>
2016-02-20 17:47:59 +0500

    Тесты - фикс Builder-а

--- build.php
+++ build.php
@@ -6,6 +6,6 @@ require 'src/Builder.php';
 require_once 'extract_phar.php';
 
 $builder = new \Pharaon\Builder();
-$builder->build('delta.phar', 'delta', 'tests/from', 'tests/to');
+$builder->build('delta.phar', 'delta', 'tests/fixtures/from', 'tests/fixtures/to');
 print('Build complete.'.PHP_EOL);
 extract_phar('./delta.phar', './phar_content');
---
+++ src/Extractor/GitBundleExtractor.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Extractor;
+
+
+class GitBundleExtractor extends AbstractExtractor
+{
+
+}
